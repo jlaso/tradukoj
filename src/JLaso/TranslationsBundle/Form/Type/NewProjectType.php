5470027c1b43ad72241e09c4cec0ee40bc175f19
joseluis laso <wld1373@gmail.com>
2013-09-25 07:50:19 +0200

    - GitHubLogin and others

---
+++ src/JLaso/TranslationsBundle/Form/Type/NewProjectType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace JLaso\TranslationsBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class NewProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('project', null, array(
+                    'attr' => array(
+                        'placeholder' => 'new_project.placeholder.project',
+                    )
+                ))
+
+
+            ->add('name', null, array(
+                    'attr' => array(
+                        'placeholder' => 'new_project.placeholder.name',
+                    )
+                ))
+
+            ->add('api_secret', null, array(
+                    'label'       => 'new_project.placeholder.api_secret',
+                    'constraints' => new NotBlank(),
+                    'attr' => array(
+                        'placeholder' => 'new_project.placeholder.api_secret',
+                    )
+                ))
+            ->add('managed_locales', null, array(
+                    'attr' => array(
+                        'placeholder' => 'new_project.placeholder.managed_locales',
+                    )
+                ))
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+                'data_class' => 'JLaso\TranslationsBundle\Entity\Project'
+            ));
+    }
+
+    public function getName()
+    {
+        return 'new_project';
+    }
+}
