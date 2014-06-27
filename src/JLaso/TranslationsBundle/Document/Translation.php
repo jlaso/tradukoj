226f2da6f88d95736a26ce6d94991f4511b8b558
joseluis laso <wld1373@gmail.com>
2014-06-27 10:40:02 +0200

    - fix error that don't updates messages by an error in date formats

--- src/JLaso/TranslationsBundle/Document/Translation.php
+++ src/JLaso/TranslationsBundle/Document/Translation.php
@@ -139,6 +139,9 @@ class Translation
         $this->updatedAt = $updatedAt;
     }
 
+    /**
+     * @return \MongoTimestamp
+     */
     public function getUpdatedAt()
     {
         return $this->updatedAt;
