a18bba5460b37152a7f1e0d3a4db42d49d4ce1e2
Joseluis Laso <joseluis@Joseluiss-Mac-mini.local>
2015-02-19 15:23:57 +0100

    some fixes in install.sh

--- app/install.sh
+++ app/install.sh
@@ -4,10 +4,11 @@ cd /vagrant
 
 sudo chmod -R 777 /tmp
 
-echo "Copying the conf of cli to apache\n"
-cp /etc/php5/cli/php.ini /etc/php5/apache2/php.ini
-echo "Restarting apache\n"
-service apache2 restart
+echo "Copying the conf of cli to apache"
+sudo cp /etc/php5/cli/php.ini /etc/php5/apache2/php.ini
+
+echo "Restarting apache"
+sudo service apache2 restart
 
 # Copies the parameters dist file to the actual parameters.yml.
 # If you run composer manually, you will benefit from Incenteev
@@ -17,11 +18,11 @@ if [ ! -e app/config/parameters.yml ]; then
     cp app/config/parameters.yml.dis app/config/parameters.yml
 fi
 
-echo "Launching composer\n"
+echo "Launching composer"
 # Firing up composer. Better to invoke the INSTALL than an UPDATE
 HOME=$(pwd) sh -c 'composer install --no-interaction'
 
-echo "Creating MYSQL database\n"
+echo "Creating MYSQL database"
 # Creating database schema and tables
 /usr/bin/env php app/console --no-interaction doctrine:database:drop --force
 /usr/bin/env php app/console --no-interaction doctrine:database:create
@@ -33,21 +34,21 @@ mysql -u root -p'root' tradukoj < app/sessionpdo.sql
 # fill language table with all languages
 mysql -u root -p'root' tradukoj < app/languages.sql
 
-echo "Creating MONGO database\n"
+echo "Creating MONGO database"
 # creating user in mongo and create schema
 echo 'db.addUser("tradukoj","tradukoj");' | mongo tradukoj
 /usr/bin/env php app/console --no-interaction doctrine:mongodb:schema:create
 
-echo "Assets & Assetic\n"
+echo "Assets & Assetic"
 # Assets & Assetic
 /usr/bin/env php app/console --no-interaction assets:install web --symlink
 /usr/bin/env php app/console --no-interaction assetic:dump
 
-echo "Creating first user and first project\n"
+echo "Creating first user and first project"
 # Allowed fixtures go here
 /usr/bin/env php app/console --no-interaction tradukoj:init:data
 
-echo "Loading translations for the first project\n"
+echo "Loading translations for the first project"
 # Load first project
 /usr/bin/env php app/console jlaso:translations:dump --force=yes
 /usr/bin/env php app/console jlaso:translations:sync --upload-first=yes
